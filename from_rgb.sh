86e1e7562b4afc213bc23c2228eedf0b5a44f30c
Enzo Vergara <vergara.enzo@gmail.com>
2017-07-01 23:15:36 +0800

    fix spacing issue for rgb

--- from_rgb.sh
+++ from_rgb.sh
@@ -1,1 +1,1 @@
-./output.sh $(echo "$1" | ruby -e 'puts STDIN.read.split(",").map{|c| (c.to_f/255.0)}.join(",")')
+./output.sh $(echo "$@" | ruby -e 'puts STDIN.read.split(",").map{|c| (c.to_f/255.0)}.join(",")' )
